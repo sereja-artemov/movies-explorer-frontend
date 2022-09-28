7467eb08609b39d6e1f7eb89c7061791dd1d0f28
sereja-art <sereja.artemov.art@yandex.ru>
2022-09-28 22:51:37 +0300

    features: TitleBlock

---
+++ src/components/Main/AboutProject/AboutProject.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TitleBlock from "../../TitleBlock/TitleBlock";
+
+const AboutProject = () => {
+    return (
+        <div>
+            <TitleBlock title="О проекте" />
+        </div>
+    );
+};
+
+export default AboutProject;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Promo from "./Promo/Promo";
+import AboutProject from "./AboutProject/AboutProject";
 
 const Main = () => {
     return (
         <main className="main-content container">
             <Promo/>
+            <AboutProject />
         </main>
     );
 }
