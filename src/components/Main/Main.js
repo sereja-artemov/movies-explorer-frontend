31eb75922aa4f6addedeee6d2f7b6bec53aacda4
sereja-art <sereja.artemov.art@yandex.ru>
2022-09-29 20:53:44 +0300

    features: Techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Promo from "./Promo/Promo";
 import AboutProject from "./AboutProject/AboutProject";
+import Techs from "./Techs/Techs";
 
 const Main = () => {
     return (
         <main className="main-content container">
             <Promo/>
             <AboutProject />
+            <Techs />
         </main>
     );
 }
---
+++ src/components/Main/Techs/Techs.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TitleBlock from "../../TitleBlock/TitleBlock";
+
+const Techs = () => {
+    return (
+        <section className="techs">
+            <TitleBlock title="Технологии" />
+            <h2 className="techs__title">7 технологий</h2>
+            <p className="techs__description">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+            <ul className="techs__card">
+                <li className="techs__card-item"><span>HTML</span></li>
+                <li className="techs__card-item"><span>CSS</span></li>
+                <li className="techs__card-item"><span>JS</span></li>
+                <li className="techs__card-item"><span>React</span></li>
+                <li className="techs__card-item"><span>Git</span></li>
+                <li className="techs__card-item"><span>Express.js</span></li>
+                <li className="techs__card-item"><span>mongoDB</span></li>
+            </ul>
+        </section>
+    );
+};
+
+export default Techs;
