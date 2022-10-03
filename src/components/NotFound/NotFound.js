a9c3fc8f468b7f98656c10cd003a1df1ab2d61b6
sereja-art <sereja.artemov.art@yandex.ru>
2022-10-03 22:29:20 +0300

    features: add 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const NotFound = () => {
     return (
-        <div>
-            sad
-        </div>
+        <section className="not-found container">
+            <h1 className="not-found__title">404</h1>
+            <p className="not-found__text">Страница не найдена</p>
+            <Link to="/" className="not-found__back">Назад</Link>
+        </section>
     );
 };
 
