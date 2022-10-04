b5e75b8295e011ae54cde82edf23f1b678d11e5f
sereja-artemov <artemov46@gmail.com>
2022-10-04 17:26:10 +0300

    features: Navigation

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+const Navigation = () => {
+    return (
+        <nav className="navigation">
+            <ul className="navigation__list">
+                <li className="navigation__list-item">
+                    <NavLink to="/" className="navigation__link" activeClassName="navigation__link--active">Главная</NavLink>
+                </li>
+                <li className="navigation__list-item">
+                    <NavLink to="/movies" className="navigation__link" activeClassName="navigation__link--active">Фильмы</NavLink>
+                </li>
+                <li className="navigation__list-item">
+                    <NavLink to="/saved-movies" className="navigation__link" activeClassName="navigation__link--active">Сохранённые фильмы</NavLink>
+                </li>
+            </ul>
+            <NavLink to="/profile" className="navigation__account-link">Аккаунт</NavLink>
+        </nav>
+    );
+};
+
+export default Navigation;
