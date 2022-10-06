20f2aff485e0ea5f73ec8a77d31bee7adbf20d05
sereja-art <sereja.artemov.art@yandex.ru>
2022-10-06 20:52:48 +0300

    style: закончил адаптивную верстку

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,20 +3,23 @@ import {NavLink} from "react-router-dom";
 
 const Navigation = ({ isOpen }) => {
     return (
-        <nav className={`navigation ${isOpen && "navigation--open"} `}>
-            <ul className="navigation__list">
-                <li className="navigation__list-item">
-                    <NavLink to="/" className="navigation__link" activeClassName="navigation__link--active">Главная</NavLink>
-                </li>
-                <li className="navigation__list-item">
-                    <NavLink to="/movies" className="navigation__link" activeClassName="navigation__link--active">Фильмы</NavLink>
-                </li>
-                <li className="navigation__list-item">
-                    <NavLink to="/saved-movies" className="navigation__link" activeClassName="navigation__link--active">Сохранённые фильмы</NavLink>
-                </li>
-            </ul>
-            <NavLink to="/profile" className="navigation__account-link">Аккаунт</NavLink>
-        </nav>
+        <>
+        <div className={`overlay ${isOpen && "overlay--active"} `}></div>
+            <nav className={`navigation ${isOpen && "navigation--open"} `}>
+                <ul className="navigation__list">
+                    <li className="navigation__list-item">
+                        <NavLink to="/" className="navigation__link" activeClassName="navigation__link--active">Главная</NavLink>
+                    </li>
+                    <li className="navigation__list-item">
+                        <NavLink to="/movies" className="navigation__link" activeClassName="navigation__link--active">Фильмы</NavLink>
+                    </li>
+                    <li className="navigation__list-item">
+                        <NavLink to="/saved-movies" className="navigation__link" activeClassName="navigation__link--active">Сохранённые фильмы</NavLink>
+                    </li>
+                </ul>
+                <NavLink to="/profile" className="navigation__account-link">Аккаунт</NavLink>
+            </nav>
+        </>
     );
 };
 
