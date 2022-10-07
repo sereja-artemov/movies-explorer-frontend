bdca0bfe9a8277e346fb52d4e0ac3a6bb0704fd7
sereja-artemov <artemov46@gmail.com>
2022-10-07 12:52:22 +0300

    fix: activeClassName

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,19 +2,26 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 
 const Navigation = ({ isOpen }) => {
+    let activeClassName = "navigation__link--active";
     return (
         <>
         <div className={`overlay ${isOpen && "overlay--active"} `}></div>
             <nav className={`navigation ${isOpen && "navigation--open"} `}>
                 <ul className="navigation__list">
                     <li className="navigation__list-item">
-                        <NavLink activeClassName="navigation__link--active" to="/" className="navigation__link" >Главная</NavLink>
+                        <NavLink to="/" className={({ isActive }) =>
+                            isActive ? `navigation__link ${activeClassName}` : "navigation__link"
+                        } end>Главная</NavLink>
                     </li>
                     <li className="navigation__list-item">
-                        <NavLink activeClassName="navigation__link--active" to="/movies" className="navigation__link" >Фильмы</NavLink>
+                        <NavLink to="/movies" className={({ isActive }) =>
+                            isActive ? `navigation__link ${activeClassName}` : "navigation__link"
+                        } >Фильмы</NavLink>
                     </li>
                     <li className="navigation__list-item">
-                        <NavLink activeClassName="navigation__link--active" to="/saved-movies" className="navigation__link" >Сохранённые фильмы</NavLink>
+                        <NavLink to="/saved-movies" className={({ isActive }) =>
+                            isActive ? `navigation__link ${activeClassName}` : "navigation__link"
+                        } >Сохранённые фильмы</NavLink>
                     </li>
                 </ul>
                 <NavLink to="/profile" className="navigation__account-link">Аккаунт</NavLink>
