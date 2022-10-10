66967a15b29232c58d75f6623554ebe3b3a26acf
sereja-artemov <artemov46@gmail.com>
2022-10-10 15:36:58 +0300

    features: Добавил запросы к бэкенду

---
+++ src/utils/MainApi.js
@@ -0,0 +1,13 @@
+export const getMovies = () => {
+  return fetch('https://api.nomoreparties.co/beatfilm-movies', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  }).then(getData);
+}
+
+function getData(res) {
+  return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+}
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "https://localhost:3005";
+export const MOVIES_SERVER_URL = "https://api.nomoreparties.co/";
