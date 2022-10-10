32af71b962b659a9c5a649011a163f821227a11f
sereja-art <sereja.artemov.art@yandex.ru>
2022-10-10 23:38:01 +0300

    features: Добавил вывод карточек в зависимости от ширины экрана

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,11 +2,11 @@ import React from "react";
 import SearchForm from "./SearchForm/SearchForm";
 import MoviesCardList from "./MoviesCardList/MoviesCardList";
 
-const Movies = ({moviesData}) => {
+const Movies = ({moviesData, windowInnerWidth}) => {
   return (
     <section className="movies">
       <SearchForm />
-      <MoviesCardList moviesData={moviesData} />
+      <MoviesCardList moviesData={moviesData} windowInnerWidth={windowInnerWidth} />
     </section>
   );
 };
--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -4,13 +4,24 @@ import Preloader from "../Preloader/Preloader";
 import { MOVIES_SERVER_URL } from "../../../utils/constants";
 import { toHoursAndMinutes } from "../../../utils/timeConverter";
 
-const MoviesCardList = ({moviesData}) => {
+const MoviesCardList = ({moviesData, windowInnerWidth}) => {
+
+  const checkCardsAmount = () => {
+   if (windowInnerWidth >= 1280) {
+     return 12
+   } else if (windowInnerWidth >= 481) {
+     return 8
+   } else if (windowInnerWidth <= 480) {
+     return 5
+   }
+  }
+
   return (
     <>
       <ul className="movies__list">
 
         { moviesData.length === 0 ? <Preloader /> :
-          moviesData.map((movie) => {
+          moviesData.slice(0, checkCardsAmount()).map((movie) => {
             return <MoviesCard
               key={movie.id}
               imgLink={MOVIES_SERVER_URL + movie.image.url}
