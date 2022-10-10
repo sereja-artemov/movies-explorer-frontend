22b23ddb3dcf506a97d34d81856a1aed6b71ddbf
sereja-art <sereja.artemov.art@yandex.ru>
2022-10-10 21:47:47 +0300

    features: Вывел список фильмов

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,11 +2,11 @@ import React from "react";
 import SearchForm from "./SearchForm/SearchForm";
 import MoviesCardList from "./MoviesCardList/MoviesCardList";
 
-const Movies = () => {
+const Movies = ({moviesData}) => {
   return (
     <section className="movies">
       <SearchForm />
-      <MoviesCardList />
+      <MoviesCardList moviesData={moviesData} />
     </section>
   );
 };
--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -2,37 +2,28 @@ import React from "react";
 import MoviesCard from "../MoviesCard/MoviesCard";
 import filmTest from "../../../images/film-test.jpg";
 import filmTest2 from "../../../images/film-test-2.jpg";
-import { moviesData } from "../../../utils/data";
 import Preloader from "../Preloader/Preloader";
+import { MOVIES_SERVER_URL } from "../../../utils/constants";
+import { toHoursAndMinutes } from "../../../utils/timeConverter";
 
-const MoviesCardList = () => {
+const MoviesCardList = ({moviesData}) => {
   return (
     <>
-      {/*<Preloader />*/}
       <ul className="movies__list">
-        { moviesData.map((movie) => {
-          return <MoviesCard
-            key={movie.id}
-            imgLink={movie.img.small.url}
-            imgAlt={movie.nameRU}
-            name={movie.nameRU}
-            duration={movie.duration}
-            isSaved="movies-card__save--saved"
-          />
-        }) }
-        <MoviesCard
-          imgLink={filmTest}
-          imgAlt="33 слова о дизайне"
-          name="33 слова о дизайне"
-          duration="1ч 17м"
-          isSaved="movies-card__save--saved"
-        />
-        <MoviesCard
-          imgLink={filmTest2}
-          imgAlt="Киноальманах «100 лет дизайна»"
-          name="Киноальманах «100 лет дизайна»"
-          duration="1ч 17м"
-        />
+
+        { moviesData.length === 0 ? <Preloader /> :
+          moviesData.map((movie) => {
+            return <MoviesCard
+              key={movie.id}
+              imgLink={MOVIES_SERVER_URL + movie.image.url}
+              imgAlt={movie.nameRU}
+              name={movie.nameRU}
+              duration={toHoursAndMinutes(movie.duration)}
+              isSaved="movies-card__save--saved"
+            />
+          })
+        }
+
       </ul>
       <button type="button" className="movies__btn btn--stroke">Еще</button>
     </>
