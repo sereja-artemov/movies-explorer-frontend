e65b1a6a46a0bdd13bc4d33bb25af03d32c842e6
sereja-art <sereja.artemov.art@yandex.ru>
2022-10-23 00:39:48 +0300

    merge

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://api.movies-expl.nomoredomains.sbs";
 export const MOVIES_SERVER_URL = "https://api.nomoreparties.co";
 
 export const SHORT_FILTER_MINUTES_DURATION = 40;
