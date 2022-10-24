b68de593f74430a2a69ba5dccca87a40f3931eb6
sereja-art <sereja.artemov.art@yandex.ru>
2022-10-24 21:11:44 +0300

    fix: добавли уведомление после изменения данных пользователя

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -39,6 +39,7 @@ function App() {
   const [filteredMovies, setFilteredMovies] = useState([]); //массив отфильтрованных фильмов
 
   const [userData, setUserData] = useState({}); //данные текущего пользователя
+  console.log(userData)
 
   const [isLoading, setIsLoading] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
